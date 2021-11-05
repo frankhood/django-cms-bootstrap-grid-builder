4209051329d10bed0064b42441c7bc72c3bc59e2
Davide Ria <d.ria@frankhood.it>
2021-11-05 11:23:45 +0100

    FRANK-863 modified conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,16 +15,16 @@ import os
 import sys
 from typing import Dict
 
+cwd = os.getcwd()
+parent = os.path.dirname(cwd)
+sys.path.append(parent)
+
 import bootstrap_grid_builder
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 # sys.path.insert(0, os.path.abspath('.'))
-
-cwd = os.getcwd()
-parent = os.path.dirname(cwd)
-sys.path.append(parent)
 
 
 # -- General configuration -----------------------------------------------------
